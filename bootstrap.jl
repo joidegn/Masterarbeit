1e65ad6ef8eea0d236882c8879f7bca77446af5d
Johannes Degn <j@degn.de>
2014-07-08 23:12:28 +0200

    updated everything

--- bootstrap.jl
+++ bootstrap.jl
@@ -3,20 +3,26 @@ using Distributions
 
 # Montecarlo-tests: used for replicating tables 2 and 3 of Breitung and Eickmeier 2011
 ### test with no structural break - meassures size of the test
-function montecarlo_test_size(R=1000, T=100, N=60, r=1, break_period=int(ceil(T/2)))
+
+function montecarlo_test_statistics(R=1000, T=100, N=60, r=1, break_period=int(ceil(T/2)), b=0)  # returns 3 arrays of RxN test statistics
     montecarlo_lrs = zeros(R, N);
     montecarlo_lms = zeros(R, N);
     montecarlo_walds = zeros(R, N);
     for rep in 1:R
         println("rep: ", rep, "(T, N): ", (T, N))
-        y, x, f, lambda, epsilon_x = factor_model_DGP(T, N, r; model="Breitung_Eickmeier_2011", b=0)
+        y, x, f, lambda, epsilon_x = factor_model_DGP(T, N, r; model="Breitung_Eickmeier_2011", b=b)
         fm = FactorModel(x, 1)  # here we could estimate the number of factors which gives strongly different results especially for small N and T
         montecarlo_lrs[rep, :] = [LR_test(fm, break_period, i) for i in 1:N]
         montecarlo_lms[rep, :] = [LM_test(fm, break_period, i) for i in 1:N]
         montecarlo_walds[rep, :] = [Wald_test(fm, break_period, i) for i in 1:N]
     end
+    return (montecarlo_lrs, montecarlo_lms, montecarlo_walds)
+end
+
+function montecarlo_test_size(R=1000, T=100, N=60, r=1, break_period=int(ceil(T/2)))
+    montecarlo_lrs, montecarlo_lms, montecarlo_walds = montecarlo_test_statistics(R, T, N, r, break_period)
     critical_value = quantile(Distributions.Chisq(r), 0.95)
-    montecarlo_lr_size = mean(sum((montecarlo_lrs.>critical_value), 2)/N)
+    montecarlo_lr_size = mean(sum(montecarlo_lrs.>critical_value, 2)/N)
     montecarlo_lm_size = mean(sum((montecarlo_lms.>critical_value), 2)/N)
     montecarlo_wald_size = mean(sum((montecarlo_walds.>critical_value), 2)/N)
     return [montecarlo_lr_size, montecarlo_lm_size, montecarlo_wald_size]
@@ -24,16 +30,7 @@ end
 
 ### test with structural break - meassures power of the test
 function montecarlo_test_power(R=1000, T=100, N=60, r=1, b=1, break_period=int(ceil(T/2)))
-    montecarlo_lrs = zeros(R, N);
-    montecarlo_lms = zeros(R, N);
-    montecarlo_walds = zeros(R, N);
-    for rep in 1:R
-        y, x, f, lambda, epsilon_x = factor_model_DGP(T, N, r; model="Breitung_Eickmeier_2011", b=b)
-        fm = FactorModel(x, 1)  # here we could estimate the number of factors which gives strongly different results especially for small N and T
-        montecarlo_lrs[rep, :] = [LR_test(fm, break_period, i) for i in 1:N]
-        montecarlo_lms[rep, :] = [LM_test(fm, break_period, i) for i in 1:N]
-        montecarlo_walds[rep, :] = [Wald_test(fm, break_period, i) for i in 1:N]
-    end
+    montecarlo_lrs, montecarlo_lms, montecarlo_walds = montecarlo_test_statistics(R, T, N, r, break_period, b)
     critical_value = quantile(Distributions.Chisq(r), 0.95)
     montecarlo_lr_size = mean(sum((montecarlo_lrs.>critical_value), 2)/N)
     montecarlo_lm_size = mean(sum((montecarlo_lms.>critical_value), 2)/N)
@@ -41,8 +38,7 @@ function montecarlo_test_power(R=1000, T=100, N=60, r=1, b=1, break_period=int(c
     return [montecarlo_lr_size, montecarlo_lm_size, montecarlo_wald_size]
 end
 
-### test with no structural break - meassures size of the test
-function bootstrap_test_size(R=1000, B=1000, T=100, N=60, r=1, break_period=int(ceil(T/2)))
+function bootstrap_test_rejections(R=1000, B=1000, T=100, N=60, r=1, break_period=int(ceil(T/2)), b=0)
     #bootstrap_lr_p_values = zeros(R, N)
     bootstrap_lr_rejections = zeros(R, N)
     #bootstrap_lm_p_values = zeros(R, N)
@@ -52,7 +48,7 @@ function bootstrap_test_size(R=1000, B=1000, T=100, N=60, r=1, break_period=int(
     critical_value = quantile(Chisq(r), 0.95)
     for rep in 1:R
         println("rep: ", rep, "(T, N): ", (T, N))
-        y, x, f, lambda, epsilon_x = factor_model_DGP(T, N, r; model="Breitung_Eickmeier_2011", b=0)
+        y, x, f, lambda, epsilon_x = factor_model_DGP(T, N, r; model="Breitung_Eickmeier_2011", b=b)
         fm = FactorModel(x, 1)  # here we could estimate the number of factors which gives strongly different results especially for small N and T
         lr_stats = [LR_test(fm, break_period, i) for i in 1:N]
         lm_stats = [LM_test(fm, break_period, i) for i in 1:N]
@@ -67,6 +63,11 @@ function bootstrap_test_size(R=1000, B=1000, T=100, N=60, r=1, break_period=int(
         #bootstrap_wald_p_values[rep, :] = [mean(bootstrap_walds[i, :].>wald_stats[i]) for i in 1:N]  # estimated p_value as in Davidson - MacKinnon p. 158 (1 - F_hat(tau_hat) = 1/B * sum(tau_start .> tau_hat))
         bootstrap_wald_rejections[rep, :] = [mean(bootstrap_walds[i, :].>critical_value) for i in 1:N]  # estimated p_value as in Davidson - MacKinnon p. 158 (1 - F_hat(tau_hat) = 1/B * sum(tau_start .> tau_hat))
     end
+    return (bootstrap_lr_rejections, bootstrap_lm_rejections, bootstrap_wald_rejections)
+end
+### test with no structural break - meassures size of the test
+function bootstrap_test_size(R=1000, B=1000, T=100, N=60, r=1, break_period=int(ceil(T/2)))
+    bootstrap_lr_rejections, bootstrap_lm_rejections, bootstrap_wald_rejections = bootstrap_test_rejections(R, B, T, N, r, break_period, 0)
     #avg_rejections_lr = mean(bootstrap_lr_p_values.<0.05)  # bootstrap_lr_p_values holds bootstrapped p_values for each variable per repetition, we average over repetitions (and variables to get something comparable to table 3)
     avg_rejections_lr = mean(bootstrap_lr_rejections)
     #avg_rejections_lm = mean(bootstrap_lm_p_values.<0.05)
@@ -78,31 +79,8 @@ end
 
 ### test with structural break - meassures power of the test
 function bootstrap_test_power(R=1000, B=1000, T=100, N=60, r=1, b=1, break_period=int(ceil(T/2)))
-    bootstrap_lr_p_values = zeros(R, N)
-    #bootstrap_lr_rejections = zeros(R, N)
-    bootstrap_lm_p_values = zeros(R, N)
-    #bootstrap_lm_rejections = zeros(R, N)
-    bootstrap_wald_p_values = zeros(R, N)
-    #bootstrap_wald_rejections = zeros(R, N)
+    bootstrap_lr_rejections, bootstrap_lm_rejections, bootstrap_wald_rejections = bootstrap_test_rejections(R, B, T, N, r, break_period, b)
     critical_value = quantile(Chisq(r), 0.95)
-    for rep in 1:R
-        println("rep: ", rep, "(T, N): ", (T, N))
-        y, x, f, lambda, epsilon_x = factor_model_DGP(T, N, r; model="Breitung_Eickmeier_2011", b=b)
-        # here we could estimate the number of factors which gives strongly different results especially for small N and T
-        fm = FactorModel(x, 1, "", "principal components", trues(size(x,2)))
-        lr_stats = [LR_test(fm, break_period, i) for i in 1:N]
-        lm_stats = [LM_test(fm, break_period, i) for i in 1:N]
-        wald_stats = [Wald_test(fm, break_period, i) for i in 1:N]
-        bootstrap_lrs = apply(hcat, [residual_bootstrap(fm, B, fm->LR_test(fm, break_period, i)) for i in 1:N])'
-        bootstrap_lms = apply(hcat, [residual_bootstrap(fm, B, fm->LM_test(fm, break_period, i)) for i in 1:N])'
-        bootstrap_walds = apply(hcat, [residual_bootstrap(fm, B, fm->Wald_test(fm, break_period, i)) for i in 1:N])'
-        bootstrap_lr_p_values[rep, :] = [mean(bootstrap_lrs[i, :].>lr_stats[i]) for i in 1:N]  # estimated p_value as in Davidson - MacKinnon p. 158 (1 - F_hat(tau_hat) = 1/B * sum(tau_start .> tau_hat))
-        #bootstrap_lr_rejections[rep, :] = [mean(bootstrap_lrs[i, :].>critical_value) for i in 1:N]
-        bootstrap_lm_p_values[rep, :] = [mean(bootstrap_lms[i, :].>lm_stats[i]) for i in 1:N]  # estimated p_value as in Davidson - MacKinnon p. 158 (1 - F_hat(tau_hat) = 1/B * sum(tau_start .> tau_hat))
-        #bootstrap_lm_rejections[rep, :] = [mean(bootstrap_lms[i, :].>critical_value) for i in 1:N]
-        bootstrap_wald_p_values[rep, :] = [mean(bootstrap_walds[i, :].>wald_stats[i]) for i in 1:N]  # estimated p_value as in Davidson - MacKinnon p. 158 (1 - F_hat(tau_hat) = 1/B * sum(tau_start .> tau_hat))
-        #bootstrap_wald_rejections[rep, :] = [mean(bootstrap_walds[i, :].>critical_value) for i in 1:N]
-    end
     avg_rejections_lr = mean(bootstrap_lr_p_values.<0.05)  # bootstrap_lr_p_values holds bootstrapped p_values for each variable per repetition, we average over repetitions (and variables to get something comparable to table 3)
     #avg_rejections_lr = mean(bootstrap_lr_rejections)  # bootstrap_lr_p_values holds bootstrapped p_values for each variable per repetition, we average over repetitions (and variables to get something comparable to table 3)
     avg_rejections_lm = mean(bootstrap_lm_p_values.<0.05)
@@ -123,6 +101,8 @@ function table2_montecarlo(R=1000, r=1, Ts=[50, 100, 150, 200], Ns=[20, 50, 100,
     end
     return table
 end
+# get all test statistic values for table 2 (to check their distribution)
+table2_stats_montecarlo(R=1000, r=1, Ts=[50, 100, 150, 200], Ns=[20, 50, 100, 150, 200]) = [montecarlo_test_statistics(R, T, N, r) for T in Ts, N in Ns]
 
 
 # replicate table 3 of Breitung, Eickmeier 2011
@@ -135,6 +115,8 @@ function table3_montecarlo(R=1000, r=1, Ts=[50, 100, 150, 200], bs=[0.1, 0.2, 0.
     end
     return table
 end
+# get all test statistic values for table 3 (to check their distribution)
+table3_stats_montecarlo(R=1000, r=1, Ts=[50, 100, 150, 200], bs=[0.1, 0.2, 0.3, 0.5], N=50) = [montecarlo_test_statistics(R, T, N, r) for T in Ts, b in bs]
 
 # Bootstrap: rerun monte-carlo of tables 2 and 3 but this time we bootstrap the tests hoping for performance increases with lower sample sizes
 # replicate table 2 of Breitung, Eickmeier 2011 - Bootstrapped
@@ -147,6 +129,7 @@ function table2_bootstrap(B=100, R=1000, r=1, Ts=[50, 100, 150, 200], Ns=[20, 50
     end
     return table
 end
+table2_stats_bootstrap(R=1000, r=1, Ts=[50, 100, 150, 200], Ns=[20, 50, 100, 150, 200]) = [bootstrap_test_statistics(R, T, N, r) for T in Ts, N in Ns]
 
 # replicate table 3 of Breitung, Eickmeier 2011 - Bootstrapped
 function table3_bootstrap(B=100, R=1000, r=1, Ts=[50, 100, 150, 200], bs=[0.1, 0.2, 0.3, 0.5], N=50)
@@ -172,7 +155,14 @@ end
 #complexity_bootstrap_table2(Ns, Ts, B=1000, R=1000) = sum([R*(3*B*Ns[i] + 3*Ns[i]) for i in 1:length(Ns), j in 1:length(Ts)])  # number of models estimated
 #complexity_bootstrap_table3(Ts, bs, B=1000, R=1000) = sum([R*(3*B*50 + 3*50) for i in 1:length(bs), j in 1:length(Ts)])  # number of models estimated
 
+Ts = [50, 100, 150, 200]
+Ns = [20, 50, 100, 150, 200]
+bs = [0.1, 0.2, 0.3, 0.5]
+
 table2_monte = table2_montecarlo()
+table2_stats_monte = table2_stats_montecarlo(1000)
 table3_monte = table3_montecarlo()
+table3_stats_monte = table3_stats_montecarlo(1000)
 table2_boot = table2_bootstrap()
 table3_boot = table3_bootstrap()
+
--- prediction.jl
+++ prediction.jl
@@ -14,18 +14,13 @@ data = readtable("/home/joi/Documents/Konstanz/Masterarbeit/data/1959-2014_norma
 data_matrix = reduce(hcat, [convert(Array{Float64}, col) for col in data.columns[2:size(data.columns, 1)]])
 #ids = map(string, names(data)[2:end])
 #titles = series_titles(ids)  # TODO: does not work at the moment because ICU.jl and with it Requests.jl seems to be broken
-T = size(data_matrix, 1) - 4  # we include 4 lags
-y = data_matrix[:,1]  # TODO: this is not something we actually want to predict...
-data_matrix = data_matrix[:, 2:end]
-lag1 = lag_vector(y)
-lag2 = lag_vector(lag1)
-lag3 = lag_vector(lag2)
-lag4 = lag_vector(lag3)
-y = y[5:end]
-w = hcat(ones(T), array(lag1[5:end]), array(lag2[5:end]), array(lag3[5:end]), array(lag4[5:end]))
-x = data_matrix[5:end, :]
+y_ind = 1
+x = data_matrix  # x includes y
 
-function pseudo_out_of_sample_forecasts(y_index=1, num_predictions::Int=10)
+BIC(mse::Float64, n::Int64, T) = log(mse) + n * log(T)/T  # Bayesian information criterion
+FPE(mse::Float64, n::Int64, T) = log(mse) + n * 2/T  # Fisher information criterion
+
+function pseudo_out_of_sample_forecasts(y_index, number_of_lags::Int64, number_of_factors::Int64; num_predictions::Int=10)
     # one step ahead pseudo out-of-sample forecasts
     T = size(x,1)
     predictions = zeros(num_predictions)
@@ -35,18 +30,53 @@ function pseudo_out_of_sample_forecasts(y_index=1, num_predictions::Int=10)
         println("month: $month")
         let y=x[1:date_index, y_index], x=x[1:date_index, [1:size(x,2)].!=y_index]  # y and x are updated so its easier for humans to read the next lines
             let newx=x[end, :], y=y[1:end-1], x=x[1:end-1, :]  # pseudo-one step ahead --> we predict only the last value in y using only information before that
-                res = FactorModel(x, "ICp1")
-                predictions[month] = predict(res, y)
+                res = FactorModel(hcat(x, y))
+                predictions[month] = predict(res, y, 1, number_of_factors)[2]
             end
         end
     end
     return(predictions, x[to_predict, y_index])
 end
 
-forecasts, true_values1 = pseudo_out_of_sample_forecasts(1, 100)
-bench_forecasts, true_values2 = benchmark_forecasts(x, 1; num_predictions=100)
+function pseudo_out_of_sample_forecasts(y_index::Int64, number_of_lags::Int64, number_of_factors::Int64, number_of_factor_lags::Int64; num_predictions::Int64=10)
+    # one step ahead pseudo out-of-sample forecasts
+    T = size(x,1)
+    predictions = zeros(num_predictions)
+    to_predict = T-num_predictions+1:T
+    for date_index in to_predict
+        month = date_index - T+num_predictions
+        println("month: $month")
+        let y=x[1:date_index, y_index], x=x[1:date_index, [1:size(x,2)].!=y_index]  # y and x are updated so its easier for humans to read the next lines
+            let newx=x[end, :], y=y[1:end-1], x=x[1:end-1, :]  # pseudo-one step ahead --> we predict only the last value in y using only information before that
+                res = DynamicFactorModel((hcat(x, y),), number_of_factor_lags)
+                predictions[month] = predict(res, y, 1, number_of_factors)[2]
+            end
+        end
+    end
+    return(predictions, x[to_predict, y_index])
+end
+
+# static factor model
+residuals = [predict(FactorModel(x, number_of_factors), x[:, y_ind], 1, number_of_lags)[1] for number_of_factors in 1:int(ceil(size(x,2)/2)), number_of_lags in 1:10]
+bics = [BIC(sum(residuals[number_of_factors, number_of_lags].^2), number_of_factors+number_of_lags, size(x, 2)-number_of_lags-1) for number_of_factors in 1:int(ceil(size(x,2)/2)), number_of_lags in 1:10]  # T is smaller because of lags and specificity of predict function
+fpes = [FPE(sum(residuals[number_of_factors, number_of_lags].^2), number_of_factors+number_of_lags, size(x, 2)-number_of_lags-1) for number_of_factors in 1:int(ceil(size(x,2)/2)), number_of_lags in 1:10]
+number_of_lags = int(ceil(indmin(bics)/size(bics, 1)))  # do indmin by hand because it doesnt seem to work with two-dimensional arrays
+number_of_factors = int(indmin(bics)-(number_of_lags-1)*size(bics, 1))
+forecasts, true_values1 = pseudo_out_of_sample_forecasts(1, number_of_lags, number_of_factors; num_predictions=100)
+bench_forecasts, true_values2 = benchmark_forecasts(x, 1; num_predictions=100, window=100)
 mse = MSE(forecasts, true_values1)
 mse_bench = MSE(bench_forecasts, true_values2)
+
+# dynamic factor model
+residuals_dyn = [predict(DynamicFactorModel((x, number_of_factors), number_of_factor_lags), x[:, y_ind], 1, number_of_lags)[1] for number_of_factors in 1:int(ceil(size(x,2)/2)), number_of_lags in 1:10, number_of_factor_lags in 1:11]
+bics_dyn = [BIC(sum(residuals[number_of_factors, number_of_lags].^2), number_of_factors+number_of_lags, size(x, 2)-number_of_lags-1) for number_of_factors in 1:int(ceil(size(x,2)/2)), number_of_lags in 1:10, number_of_factor_lags in 1:11]  # T is smaller because of lags and specificity of predict function
+fpes_dyn = [FPE(sum(residuals[number_of_factors, number_of_lags].^2), number_of_factors+number_of_lags, size(x, 2)-number_of_lags-1) for number_of_factors in 1:int(ceil(size(x,2)/2)), number_of_lags in 1:10]
+# do indmin by hand because it doesnt seem to work with 3-dimensional arrays
+number_of_factor_lags_dyn = int(ceil(indmin(bics_dyn)/(size(bics_dyn, 1)*size(bics_dyn, 2))))  # third dimension we are in is index number divided by number of elements in first and second dimension combined (which is the number of elements "per third dimension")
+number_of_lags_dyn = int(ceil((indmin(bics_dyn)-(number_of_factor_lags_dyn-1)*size(bics_dyn, 1)*size(bics_dyn, 2))/size(bics_dyn, 1)))  # second dimension is then (index number - elements left if we deduct the elements from the third dimension) / elements in first dimension
+number_of_factors_dyn = int(indmin(bics_dyn)-(number_of_factor_lags_dyn-1)*size(bics_dyn, 2)*size(bics_dyn, 1)-(number_of_lags_dyn-1)*size(bics_dyn, 1))  # first dimension is then again what is left if we account for third and second
+forecasts_dyn, true_values1 = pseudo_out_of_sample_forecasts(1, number_of_lags_dyn, number_of_factors_dyn, number_of_factor_lags_dyn; num_predictions=100)
+mse_dyn = MSE(forecasts_dyn, true_values1)
 
 #predictions_frame = DataFrame(
 #    predictions=vcat(y[end-199:end], predictions, predictions_targeted, predictions_ols, predictions_average),
